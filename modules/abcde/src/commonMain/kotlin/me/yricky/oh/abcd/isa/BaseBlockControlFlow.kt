e75fb70d0a5246dcfb130cb0fde499bdef00769b
dd <dd@dddeMacBook-Pro.local>
2024-12-05 22:38:18 +0800

    add CFG

--- modules/abcde/src/commonMain/kotlin/me/yricky/oh/abcd/isa/BaseBlockControlFlow.kt
+++ modules/abcde/src/commonMain/kotlin/me/yricky/oh/abcd/isa/BaseBlockControlFlow.kt
@@ -45,7 +45,7 @@ class BaseBlockControlFlow(
         }
         return null
     }
-
+    val basicBlockMap = mutableMapOf<Int, BaseBlock>()
     // 获取正确的基本块列表 还没有连接信息
     fun generaStep1() : ArrayList<BaseBlock>{
         val start_flags = ArrayList<Number>()
@@ -89,6 +89,7 @@ class BaseBlockControlFlow(
             bbk.addAsm(_asm)
             if(_asm.codeOffset in end_flags){
                 li.add(bbk)
+                basicBlockMap.put(bbk.offset,bbk)
             }
         }
         return li
@@ -117,38 +118,78 @@ class BaseBlockControlFlow(
 
 
     }
-    // 生成有向图
-    fun generaStep3() {
-        baseBlocks.forEach {
-            baseBlockCF.addNode(it)
-        }
-        baseBlockPath.forEach { (_off, next_asm) ->
-            baseBlockOffAsm.get(_off)?.let{ cur_asm ->
-                val cur_bbk = findBBKbyAsm(cur_asm)
-                val next_bbk = findBBKbyAsm(next_asm)
-                if(cur_bbk != null && next_bbk != null&& cur_bbk != next_bbk){
-                    if(cur_bbk.name == "Normal") {
-                        val cur_idx =  revBaseblocks.get(cur_bbk)
-                        val near_bbk = cur_idx?.let { baseBlocks.get(it + 1) }
-//                            println("${cur_bbk.offset} --- ${near_bbk?.let { it.offset }}")
-                        baseBlockCF.putEdgeValue(cur_bbk, near_bbk, true)
-                        baseBlockCF.putEdgeValue(cur_bbk, next_bbk, true)
-                    }
-                    else if(cur_bbk.name == "Final") {
 
-                    }
-                    else if(cur_bbk.name == "Jmp"){
-                        baseBlockCF.putEdgeValue(cur_bbk, next_bbk, true)
-                    }
+    /**
+     * 生成有向图
+     */
+    val conditionJmp = arrayOf("jeqz", "jnez")
+    val directJmp = arrayOf("jmp")
+    val returnAsm = arrayOf("return", "returnundefined")
+
+    fun generaStep3(){
+        basicBlockMap.forEach{(_off, bbk) ->
+            baseBlockCF.addNode(bbk)
+            val terminator = bbk.getTermiinator()
+            if(terminator.asmName in conditionJmp){//条件跳转
+                val dest = terminator.codeOffset + terminator.opUnits[1].toInt()
+                val fall = terminator.nextOffset
+                val destNode = basicBlockMap.get(dest)
+                val fallNode = basicBlockMap.get(fall)
+                baseBlockCF.addNode(destNode)
+                baseBlockCF.addNode(fallNode)
+                baseBlockCF.putEdgeValue(bbk,destNode,false)
+                baseBlockCF.putEdgeValue(bbk,fallNode,true)
+            }else if(terminator.asmName in directJmp){//直接跳转
+                val dest = terminator.codeOffset + terminator.opUnits[1].toInt()
+                val destNode = basicBlockMap.get(dest)
+                baseBlockCF.addNode(destNode)
+                baseBlockCF.putEdgeValue(bbk,destNode,true)
+            }else if(terminator.asmName in returnAsm){
+                //返回语句，无fallNode，无须处理
+            }else{//其他情况，顺序执行
+                val fall = terminator.nextOffset
+                //若当前块是最后一个指令，则不添加fallNode
+                if(fall < basicBlockMap.entries.last().value.getTermiinator().codeOffset){
+                    val fallNode = basicBlockMap.get(fall)
+                    baseBlockCF.addNode(fallNode)
+                    baseBlockCF.putEdgeValue(bbk,fallNode,true)
                 }
-
             }
         }
-
-        baseBlockCF.edges().forEach { edge ->
-            println("${edge.source().offset} -> ${edge.target().offset}")
-        }
     }
+
+    // 生成有向图
+//    fun generaStep3() {
+//        baseBlocks.forEach {
+//            baseBlockCF.addNode(it)
+//        }
+//        baseBlockPath.forEach { (_off, next_asm) ->
+//            baseBlockOffAsm.get(_off)?.let{ cur_asm ->
+//                val cur_bbk = findBBKbyAsm(cur_asm)
+//                val next_bbk = findBBKbyAsm(next_asm)
+//                if(cur_bbk != null && next_bbk != null&& cur_bbk != next_bbk){
+//                    if(cur_bbk.name == "Normal") {
+//                        val cur_idx =  revBaseblocks.get(cur_bbk)
+//                        val near_bbk = cur_idx?.let { baseBlocks.get(it + 1) }
+////                            println("${cur_bbk.offset} --- ${near_bbk?.let { it.offset }}")
+//                        baseBlockCF.putEdgeValue(cur_bbk, near_bbk, true)
+//                        baseBlockCF.putEdgeValue(cur_bbk, next_bbk, true)
+//                    }
+//                    else if(cur_bbk.name == "Final") {
+//
+//                    }
+//                    else if(cur_bbk.name == "Jmp"){
+//                        baseBlockCF.putEdgeValue(cur_bbk, next_bbk, true)
+//                    }
+//                }
+//
+//            }
+//        }
+//
+//        baseBlockCF.edges().forEach { edge ->
+//            println("${edge.source().offset} -> ${edge.target().offset}")
+//        }
+//    }
     // 有向图 -> CFG
     fun generaStep4(){
 
@@ -162,6 +203,14 @@ class BaseBlockControlFlow(
             if(li.size > 0)
                 li[0].codeOffset
             else -1
+        }
+
+        fun getTermiinator(): Asm.AsmItem{
+            return li.last()
+        }
+
+        fun getName1(): String{
+            return offset.toString()
         }
         fun addAsm(asm: AsmItem): Boolean {
 //            if(asm.codeOffset == 0)
